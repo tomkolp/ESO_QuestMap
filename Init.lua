6bfdc64de5a86cde0715352c1e2b58ac944ff5aa
Sharlikran <danielhmpdx16@gmail.com>
2020-05-13 19:21:23 -0700

    Update to adding third icon set

--- Init.lua
+++ Init.lua
@@ -30,8 +30,8 @@ QuestMap.pinTypes = {
 
 QuestMap.iconSets = {
     QuestMap = {"QuestMap/icons/pinQuestUncompleted.dds", "QuestMap/icons/pinQuestCompleted.dds"},
-    ESO = {"/esoui/art/floatingmarkers/quest_available_icon.dds", "/esoui/art/icons/achievements_indexicon_quests_down.dds"},
-    -- ESOInverted = {"QuestMap/icons/eso_inverted_uncompleted.dds", "QuestMap/icons/eso_inverted_completed.dds"},
+    ESO = {"esoui/art/floatingmarkers/quest_available_icon.dds", "esoui/art/icons/achievements_indexicon_quests_down.dds"},
+    ESOInverted = {"QuestMap/icons/eso_inverted_uncompleted.dds", "QuestMap/icons/eso_inverted_completed.dds"},
 }
 
 QuestMap.settings_default = {
