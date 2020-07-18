bdd476e5743af74760a95085c8eb204e712e43fa
Fenixs <58373016+FenixsRU@users.noreply.github.com>
2020-07-19 00:49:48 +0500

    Update Strings-ru.lua

--- lang/Strings-ru.lua
+++ lang/Strings-ru.lua
@@ -8,126 +8,126 @@ https://github.com/CaptainBlagbird
 
 local strings = {
     -- General
-    QUESTMAP_UNCOMPLETED            = "Uncompleted",
-    QUESTMAP_COMPLETED              = "Completed",
-    QUESTMAP_HIDDEN                 = "Manually hidden",
-    QUESTMAP_STARTED                = "Started",
-    QUESTMAP_REPEATABLE             = "Repeatable",
-    QUESTMAP_DAILY                  = "Daily",
-    QUESTMAP_CADWELL                = "Cadwell",
-    QUESTMAP_SKILL                  = "Skill point",
+    QUESTMAP_UNCOMPLETED            = "Не завершённые",
+    QUESTMAP_COMPLETED              = "Завершённые",
+    QUESTMAP_HIDDEN                 = "Скрытые вручную",
+    QUESTMAP_STARTED                = "Стартовые",
+    QUESTMAP_REPEATABLE             = "Повторяемые",
+    QUESTMAP_DAILY                  = "Ежедневные",
+    QUESTMAP_CADWELL                = "Кэдвелл",
+    QUESTMAP_SKILL                  = "Очки навыков",
 
-    QUESTMAP_HIDE                   = "Hide quest",
-    QUESTMAP_UNHIDE                 = "Unhide quest",
+    QUESTMAP_HIDE                   = "Скрыть задание",
+    QUESTMAP_UNHIDE                 = "Раскрыть задание",
 
-    QUESTMAP_MSG_HIDDEN             = "Quest hidden",
-    QUESTMAP_MSG_UNHIDDEN           = "Quest unhidden",
-    QUESTMAP_MSG_HIDDEN_P           = "Quests hidden",
-    QUESTMAP_MSG_UNHIDDEN_P         = "Quests unhidden",
+    QUESTMAP_MSG_HIDDEN             = "Задание скрыто",
+    QUESTMAP_MSG_UNHIDDEN           = "Задание раскрыто",
+    QUESTMAP_MSG_HIDDEN_P           = "Задания скрыты",
+    QUESTMAP_MSG_UNHIDDEN_P         = "Задания раскрыты",
 
-    QUESTMAP_QUESTS                 = "Quests",
-    QUESTMAP_QUEST_SUBFILTER        = "Subfilter",
+    QUESTMAP_QUESTS                 = "Задания",
+    QUESTMAP_QUEST_SUBFILTER        = "Подфильтр",
 
-    QUESTMAP_SLASH_USAGE            = "Please use an argument after the command:\n 'hide' - Hide all quests on the current map\n 'unhide' - Unhide all quests on the current map",
-    QUESTMAP_SLASH_MAPINFO          = "Please open the map first.",
+    QUESTMAP_SLASH_USAGE            = "Пожалуйста, используйте аргумент после команды:\n 'hide' - Скрыть все задания на текущей карте\n 'unhide' - Раскрыть все задания на текущей карте",
+    QUESTMAP_SLASH_MAPINFO          = "В начале, откройте пожалуйста карту.",
 
-    QUESTMAP_LIB_REQUIRED           = "not installed.",
+    QUESTMAP_LIB_REQUIRED           = "не установлена.",
 
     -- Settings menu
-    QUESTMAP_MENU_ICON_SET          = "Icon set",
-    QUESTMAP_MENU_REPEATABLE_ICON_SET        = "Repeatable Icon set",
+    QUESTMAP_MENU_ICON_SET          = "Набор иконок",
+    QUESTMAP_MENU_REPEATABLE_ICON_SET        = "Повторяемый Набор иконок",
 
-    QUESTMAP_MENU_PIN_SIZE          = "Map pin size",
-    QUESTMAP_MENU_PIN_SIZE_TT       = "Defines the display size of the map pins (default: "..QuestMap.settings_default.pinSize..")",
+    QUESTMAP_MENU_PIN_SIZE          = "Размер метки на карте",
+    QUESTMAP_MENU_PIN_SIZE_TT       = "Определяет размер отображаемых на карте меток (по умолчанию: "..QuestMap.settings_default.pinSize..")",
 
-    QUESTMAP_MENU_PIN_LVL           = "Map pin level",
-    QUESTMAP_MENU_PIN_LVL_TT        = "Defines on which level the map pins are drawn (default: "..QuestMap.settings_default.pinLevel..")",
+    QUESTMAP_MENU_PIN_LVL           = "Уровень метки на карте",
+    QUESTMAP_MENU_PIN_LVL_TT        = "Определяет на каком уровне находятся метки на  карте (по умолчанию: "..QuestMap.settings_default.pinLevel..")",
 
-    QUESTMAP_MENU_DISP_MSG          = "Display quest hidden or unhidden chat notification",
-    QUESTMAP_MENU_DISP_MSG_TT       = "Enable or disable chat window notification when hiding or unhiding pins",
+    QUESTMAP_MENU_DISP_MSG          = "Показывать уведомление скрытие или раскрытие задание в чате",
+    QUESTMAP_MENU_DISP_MSG_TT       = "Включить или отключить уведомление в окне чата при скрытии или раскрытии меток",
 
-    QUESTMAP_MENU_TOGGLE_HIDDEN_MSG       = "Toggle option to hide or unhide Quests",
-    QUESTMAP_MENU_TOGGLE_HIDDEN_MSG_TT    = "Enable or disable option to hide or unhide quests when you click a quest pin.",
+    QUESTMAP_MENU_TOGGLE_HIDDEN_MSG       = "Настройка скрытия/раскрытия заданий",
+    QUESTMAP_MENU_TOGGLE_HIDDEN_MSG_TT    = "Включите или отключите эту опцию, чтобы скрывать/раскрывать задания, когда вы кликаете на метку задания.",
 
-    QUESTMAP_MENU_TOGGLE_COMPLETED_MSG       = "Toggle option to show completed quest list",
-    QUESTMAP_MENU_TOGGLE_COMPLETED_MSG_TT    = "Enable or disable option to show quest list when you click a completed quest pin and pins are stacked on top one another.",
+    QUESTMAP_MENU_TOGGLE_COMPLETED_MSG       = "Показа списка при завершённом задании",
+    QUESTMAP_MENU_TOGGLE_COMPLETED_MSG_TT    = "Включите или отключите эту опцию для показа списка заданий при клике на метку завершённого задания если она перекрывает другую.",
 
-    QUESTMAP_MENU_HIDDEN_QUESTS_T   = "Manually hide quests",
-    QUESTMAP_MENU_HIDDEN_QUESTS_1   = "You can manually (un)hide quest pins by clicking on them. (To see hidden quest pins, activate the map pin filter to the right of the map.)",
-    QUESTMAP_MENU_HIDDEN_QUESTS_2   = "To (un)hide all quest pins in a map at once, you can use the chat command '/qm hide' or '/qm unhide'.",
-    QUESTMAP_MENU_HIDDEN_QUESTS_B   = "If you want to clear ALL manually hidden quest pins at once, you can use this button:",
+    QUESTMAP_MENU_HIDDEN_QUESTS_T   = "Вручную скрытые задания",
+    QUESTMAP_MENU_HIDDEN_QUESTS_1   = "Вы можете вручную скрыть/раскрыть метку задания кликнув на неё.(Чтобы увидеть скрытые метки заданий, включите соответствующий фильтр меток справа от карты.)",
+    QUESTMAP_MENU_HIDDEN_QUESTS_2   = "Чтобы скрыть/раскрыть сразу все метки заданий на карте, вы можете использовать в чате команду '/qm hide' или '/qm unhide'.",
+    QUESTMAP_MENU_HIDDEN_QUESTS_B   = "Если вы хотите сразу восстановить ВСЕ скрытые вручную метки заданий, вы можете использовать эту кнопку:",
 
-    QUESTMAP_MENU_RESET_HIDDEN      = "Reset hidden pins",
-    QUESTMAP_MENU_RESET_HIDDEN_TT   = "Reset manually hidden pins",
-    QUESTMAP_MENU_RESET_HIDDEN_W    = "Cannot be undone!",
+    QUESTMAP_MENU_RESET_HIDDEN      = "Восстановить скрытые метки",
+    QUESTMAP_MENU_RESET_HIDDEN_TT   = "Восстанавливает все скрытые вручную метки",
+    QUESTMAP_MENU_RESET_HIDDEN_W    = "Невозможно отменить!",
 
-    QUESTMAP_MENU_RESET_NOTE        = "Note: Clicking on '"..GetString(SI_OPTIONS_DEFAULTS).."' below does NOT reset manually hidden quest pins.",
+    QUESTMAP_MENU_RESET_NOTE        = "Заметка: Кликнув на '"..GetString(SI_OPTIONS_DEFAULTS).."' расположенном ниже, вы НЕ ВОСТАНОВИТЕ скрытые вручную метки заданий.",
 
-    QUESTMAP_MENU_SHOW_SUFFIX        = "Display tooltip suffix",
-    QUESTMAP_MENU_SHOW_SUFFIX_TT     = "Toggle quest pin tooltip suffix for personal preference or for color blind accessibility.",
+    QUESTMAP_MENU_SHOW_SUFFIX        = "Отображать суффикс в подсказках",
+    QUESTMAP_MENU_SHOW_SUFFIX_TT     = "Отображать или нет суффикс в подсказке к меткам заданий для дальтоников или по личным предпочтениям.",
 
     -- Uncompleted quest pin text
-    QUESTMAP_UNCOMPLETED_PIN_COLOR  = "Uncompleted Quest Pin Color",
-    QUESTMAP_UNCOMPLETED_PIN_COLOR_DESC  = "Change Uncompleted Quest Pin Color",
+    QUESTMAP_UNCOMPLETED_PIN_COLOR  = "Цвет меток не завершённых заданий",
+    QUESTMAP_UNCOMPLETED_PIN_COLOR_DESC  = "Смена цвета меток не завершённых заданий",
 
-    QUESTMAP_UNCOMPLETED_TOOLTIP_COLOR  = "Uncompleted Quest Tooltip Color",
-    QUESTMAP_UNCOMPLETED_TOOLTIP_COLOR_DESC  = "Change Uncompleted Quest Tooltip Color",
+    QUESTMAP_UNCOMPLETED_TOOLTIP_COLOR  = "Цвет подсказок не завершённых заданий",
+    QUESTMAP_UNCOMPLETED_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок не завершённых заданий",
 
     -- Completed quest pin text
-    QUESTMAP_COMPLETED_PIN_COLOR  = "Completed Quest Pin Color",
-    QUESTMAP_COMPLETED_PIN_COLOR_DESC  = "Change Completed Quest Pin Color",
+    QUESTMAP_COMPLETED_PIN_COLOR  = "Цвет меток завершённых заданий",
+    QUESTMAP_COMPLETED_PIN_COLOR_DESC  = "Смена цвета меток завершённых заданий",
 
-    QUESTMAP_COMPLETED_TOOLTIP_COLOR  = "Completed Quest Tooltip Color",
-    QUESTMAP_COMPLETED_TOOLTIP_COLOR_DESC  = "Change Completed Quest Tooltip Color",
+    QUESTMAP_COMPLETED_TOOLTIP_COLOR  = "Цвет подсказок завершённых заданий",
+    QUESTMAP_COMPLETED_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок завершённых заданий",
 
     -- Hidden quest pin text
-    QUESTMAP_HIDDEN_PIN_COLOR  = "Hidden Quest Pin Color",
-    QUESTMAP_HIDDEN_PIN_COLOR_DESC  = "Change Hidden Quest Pin Color",
+    QUESTMAP_HIDDEN_PIN_COLOR  = "Цвет меток скрытых заданий",
+    QUESTMAP_HIDDEN_PIN_COLOR_DESC  = "Смена цвета меток скрытых заданий",
 
-    QUESTMAP_HIDDEN_TOOLTIP_COLOR  = "Hidden Quest Tooltip Color",
-    QUESTMAP_HIDDEN_TOOLTIP_COLOR_DESC  = "Change Hidden Quest Tooltip Color",
+    QUESTMAP_HIDDEN_TOOLTIP_COLOR  = "Цвет подсказок скрытых заданий",
+    QUESTMAP_HIDDEN_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок скрытых заданий",
 
     -- Started quest pin text
-    QUESTMAP_STARTED_PIN_COLOR  = "Started Quest Pin Color",
-    QUESTMAP_STARTED_PIN_COLOR_DESC  = "Change Started Quest Pin Color",
+    QUESTMAP_STARTED_PIN_COLOR  = "Цвет меток стартовых заданий",
+    QUESTMAP_STARTED_PIN_COLOR_DESC  = "Смена цвета меток стартовых заданий",
 
-    QUESTMAP_STARTED_TOOLTIP_COLOR  = "Started Quest Tooltip Color",
-    QUESTMAP_STARTED_TOOLTIP_COLOR_DESC  = "Change Started Quest Tooltip Color",
+    QUESTMAP_STARTED_TOOLTIP_COLOR  = "Цвет подсказок стартовых заданий",
+    QUESTMAP_STARTED_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок стартовых заданий",
 
     -- Repeatable quest pin text
-    QUESTMAP_REPEATABLE_PIN_COLOR  = "Repeatable Quest Pin Color",
-    QUESTMAP_REPEATABLE_PIN_COLOR_DESC  = "Change Repeatable Quest Pin Color",
+    QUESTMAP_REPEATABLE_PIN_COLOR  = "Цвет меток повторяемых заданий",
+    QUESTMAP_REPEATABLE_PIN_COLOR_DESC  = "Смена цвета меток повторяемых заданий",
 
-    QUESTMAP_REPEATABLE_TOOLTIP_COLOR  = "Repeatable Quest Tooltip Color",
-    QUESTMAP_REPEATABLE_TOOLTIP_COLOR_DESC  = "Change Repeatable Quest Tooltip Color",
+    QUESTMAP_REPEATABLE_TOOLTIP_COLOR  = "Цвет подсказок повторяемых заданий",
+    QUESTMAP_REPEATABLE_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок повторяемых заданий",
 
     -- Daily quest pin text
-    QUESTMAP_DAILY_PIN_COLOR  = "Daily Quest Pin Color",
-    QUESTMAP_DAILY_PIN_COLOR_DESC  = "Change Daily Quest Pin Color",
+    QUESTMAP_DAILY_PIN_COLOR  = "Цвет меток ежедневных заданий",
+    QUESTMAP_DAILY_PIN_COLOR_DESC  = "Смена цвета меток ежедневных заданий",
 
-    QUESTMAP_DAILY_TOOLTIP_COLOR  = "Daily Quest Tooltip Color",
-    QUESTMAP_DAILY_TOOLTIP_COLOR_DESC  = "Change Daily Quest Tooltip Color",
+    QUESTMAP_DAILY_TOOLTIP_COLOR  = "Цвет подсказок ежедневных заданий",
+    QUESTMAP_DAILY_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок ежедневных заданий",
 
     -- Cadwell quest pin text
-    QUESTMAP_CADWELL_PIN_COLOR  = "Cadwell Quest Pin Color",
-    QUESTMAP_CADWELL_PIN_COLOR_DESC  = "Change Cadwell Quest Pin Color",
+    QUESTMAP_CADWELL_PIN_COLOR  = "Цвет меток заданий Кэдвелла",
+    QUESTMAP_CADWELL_PIN_COLOR_DESC  = "Смена цвета меток заданий Кэдвелла",
 
-    QUESTMAP_CADWELL_TOOLTIP_COLOR  = "Cadwell Quest Tooltip Color",
-    QUESTMAP_CADWELL_TOOLTIP_COLOR_DESC  = "Change Cadwell Quest Tooltip Color",
+    QUESTMAP_CADWELL_TOOLTIP_COLOR  = "Цвет подсказок заданий Кэдвелла",
+    QUESTMAP_CADWELL_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок заданий Кэдвелла",
 
     -- Skill quest pin text
-    QUESTMAP_SKILL_PIN_COLOR  = "Skill Quest Pin Color",
-    QUESTMAP_SKILL_PIN_COLOR_DESC  = "Change Skill Quest Pin Color",
+    QUESTMAP_SKILL_PIN_COLOR  = "Цвет меток заданий на очки навыков",
+    QUESTMAP_SKILL_PIN_COLOR_DESC  = "Смена цвета меток заданий на очки навыков",
 
-    QUESTMAP_SKILL_TOOLTIP_COLOR  = "Skill Quest Tooltip Color",
-    QUESTMAP_SKILL_TOOLTIP_COLOR_DESC  = "Change Skill Quest Tooltip Color",
+    QUESTMAP_SKILL_TOOLTIP_COLOR  = "Цвет подсказок заданий на очки навыков",
+    QUESTMAP_SKILL_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок заданий на очки навыков",
 
     -- Dungeon quest pin text
-    QUESTMAP_DUNGEON_PIN_COLOR  = "Dungeon Quest Pin Color",
-    QUESTMAP_DUNGEON_PIN_COLOR_DESC  = "Change Dungeon Quest Pin Color",
+    QUESTMAP_DUNGEON_PIN_COLOR  = "Цвет меток заданий для подземелий",
+    QUESTMAP_DUNGEON_PIN_COLOR_DESC  = "Смена цвета меток заданий для подземелий",
 
-    QUESTMAP_DUNGEON_TOOLTIP_COLOR  = "Dungeon Quest Tooltip Color",
-    QUESTMAP_DUNGEON_TOOLTIP_COLOR_DESC  = "Change Dungeon Quest Tooltip Color",
+    QUESTMAP_DUNGEON_TOOLTIP_COLOR  = "Цвет подсказок заданий для подземелий",
+    QUESTMAP_DUNGEON_TOOLTIP_COLOR_DESC  = "Смена цвета подсказок заданий для подземелий",
 
 }
 
